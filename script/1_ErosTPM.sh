63a43ec79149c5d02aa73e4958a129bc5a7d77df
Jin Beniyama <jinbeniyama@gmail.com>
2025-08-05 14:40:44 +0200

    Update for final TPMs

--- script/1_ErosTPM.sh
+++ script/1_ErosTPM.sh
@@ -3,7 +3,8 @@
 # NOTE: 
 #   - Color-term correction was already done.
 #   - runtpm doesnt work when low thermal intertia is too low.
-#     add after '-p 3600'. when TI < 30.
+#     Ddd after '-p 3600'. when TI < 30.
+#   - Use Lagerros approximation
 
 
 # Object (for output files)
@@ -33,17 +34,15 @@ BondA=0.12
 
 # Free parameters =============================================================
 # Thermal intertia
-TIarr=(0 10 20 30 40 50 60 70 80 90 100 
-       150 200 250 300 350 400 450 500 550 600 
-       650 700 750 800 850 900 950 1000
-       1100 1200 1300 1400 1500 1600 1700 1800 1900 2000
-       2100 2200 2300 2400 2500)
+TIarr=(10 20 30 40 50 60 70 80 90 100
+       150 200 250 300 350 400 450 500 550 600
+       650 700 750 800 850 900 950 1000)
 
 # Roughness (crater) parameters
 # We do not fix this. We use roughness in the literature for validation purposes.
-# The same as Hung+2022, PSJ
-CAarr=(0    30  40  41  50  60  70  88  89  90)
-CRarr=(0.0 0.3 0.7 0.9 0.5 0.9 0.7 0.5 0.7 0.9)
+# We only change CR to convert to RMS slope later.
+CAarr=(90   90  90  90  90  90  90  90  90  90  90)
+CRarr=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
 # Free parameters =============================================================
 
 
@@ -77,11 +76,10 @@ do
     # wo/-f: Lagerros approximation 
     # w/-f : Full heat diffusion within the craters, much slower, but safer, more physical
     # Just output the command
-    echo 'Command: echo'  ${fOBJ} ${fEPH} ${EPS} ${TI} ${BondA} ${CA} ${CR} '| runtpm -f -S' ${fSPIN} '-o' ${fOBS}  '-p' ${NPHASE} '>' ${OUTDIR}/tpmout_${OBJ}_brute_ti${TI}_ca${CA}_cr${CR}.dat '&'
+    echo 'Command: echo'  ${fOBJ} ${fEPH} ${EPS} ${TI} ${BondA} ${CA} ${CR} '| runtpm -S' ${fSPIN} '-o' ${fOBS}  '-p' ${NPHASE} '>' ${OUTDIR}/tpmout_${OBJ}_brute_ti${TI}_ca${CA}_cr${CR}.dat '&'
     echo ""
     # Start TPM!
-        echo ${fOBJ} ${fEPH} ${EPS} ${TI} ${BondA} ${CA} ${CR} | runtpm -f -S ${fSPIN} -o ${fOBS} -p ${NPHASE} > ${OUTDIR}/tpmout_${OBJ}_brute_ti${TI}_ca${CA}_cr${CR}.dat &
-        #echo ${fOBJ} ${fEPH} ${EPS} ${TI} ${BondA} ${CA} ${CR} | runtpm -S ${fSPIN} -o ${fOBS} -p ${NPHASE} > ${OUTDIR}/tpmout_${OBJ}_brute_ti${TI}_ca${CA}_cr${CR}.dat &
+        echo ${fOBJ} ${fEPH} ${EPS} ${TI} ${BondA} ${CA} ${CR} | runtpm -S ${fSPIN} -o ${fOBS} -p ${NPHASE} > ${OUTDIR}/tpmout_${OBJ}_brute_ti${TI}_ca${CA}_cr${CR}.dat &
     done
 	# Wait until all 10 processes are finished
 	wait
